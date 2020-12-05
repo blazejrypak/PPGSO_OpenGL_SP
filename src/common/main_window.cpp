4065673cef78774988b5402f633446a692a9e8cf
Blazej <rypak.b@gmail.com>
2020-12-05 20:56:28 +0100

    added cars with collisions between them

--- src/common/main_window.cpp
+++ src/common/main_window.cpp
@@ -31,7 +31,7 @@ SceneWindow::SceneWindow () : Window{"Apartment", SIZE, SIZE} {
     glFrontFace(GL_CCW);
     glCullFace(GL_BACK);
     
-    this->openMenu();
+    this->openDayScene();
 }
 
 void SceneWindow::onKey (int key, int scanCode, int action, int mods) {
